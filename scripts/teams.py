947fd45253ad585f2347e5cc0c556c562f2e599b
hoangnhavo17 <51522857+hoangnhavo17@users.noreply.github.com>
2025-07-19 09:25:40 -0500

    Update teams.py

--- scripts/teams.py
+++ scripts/teams.py
@@ -1,1 +1,17 @@
+import json
+from nba_api.stats.static import teams
 
+nba_teams = teams.get_teams()
+
+team_map = {
+    team['full_name']: {
+        'abbreviation': team['abbreviation'],
+        'nickname': team['nickname'],
+        'city': team['city'],
+        'id': team['id']
+    }
+    for team in nba_teams
+}
+
+with open("../public/teams.json", "w") as f:
+    json.dump(team_map, f, indent=2)
